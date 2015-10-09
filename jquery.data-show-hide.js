d9dc29668132c8438e55e4ccd0d0588b67c83707
Kaspars <kasparsj@gmail.com>
2015-10-09 20:21:15 +0300

    test passing now

--- jquery.data-show-hide.js
+++ jquery.data-show-hide.js
@@ -2,23 +2,47 @@
     'use strict';
 
     $(document.body)
-        .on("change", "input[data-show]:checked", function() {
-            var $this = $(this);
-            getToggleElement($this, "show").show();
-        })
-        .on("change", "input[data-hide]:checked", function() {
-            var $this = $(this);
-            getToggleElement($this, "hide").hide();
-        })
-        .on("change", "select[data-toggle]", function() {
+        .on("change", "input[data-show]", function() {
             var $this = $(this),
-                $toggle = getToggleElement($this, "toggle"),
-                value = $this.data("toggleValue");
-            $toggle[($this.val() == value ? 'show' : 'hide')]();
+                $showEl = getDataElement($this, "show");
+            if ($showEl)
+                $showEl[$this.is(":checked") ? "show" : "hide"]();
+        })
+        .on("change", "input[data-hide]", function() {
+            var $this = $(this),
+                $hideEl = getDataElement($this, "hide");
+            if ($hideEl) {
+                $hideEl[$this.is(":checked") ? "hide" : "show"]();
+            }
+        })
+        .on("change", "input[data-toggle]", function() {
+            var $this = $(this),
+                $toggleEl = getDataElement($this, "toggle");
+            if ($toggleEl) {
+                $toggleEl.toggle();
+            }
+        })
+        .on("change", "select:has(option[data-show],option[data-hide],option[data-toggle])", function() {
+            var $selected = $("option:selected", this),
+                $showEl = getDataElement($selected, "show"),
+                $hideEl = getDataElement($selected, "hide"),
+                $toggleEl = getDataElement($selected, "toggle");
+            if ($showEl) {
+                $showEl.show();
+            }
+            if ($hideEl) {
+                $hideEl.hide();
+            }
+            if ($toggleEl) {
+                $toggleEl.toggle();
+            }
         });
 
-    function getToggleElement($input, prop) {
-        return $($input.data(prop))
+    function getDataElement($input, prop) {
+        var selector = $input.data(prop);
+        if (typeof selector == "string") {
+            return $(selector);
+        }
     }
 
 })(jQuery);
